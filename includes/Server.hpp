f737f417d924dd01c4bbe98f42ab868c7f4d9f61
Paula Balbino <lola.paulinha@gmail.com>
2024-07-13 10:13:29 +0400

    Adding Server.cpp and .hpp

--- includes/Server.hpp
+++ includes/Server.hpp
@@ -1,22 +1,64 @@
 #ifndef SERVER_HPP
 #define SERVER_HPP
 
-# include <iostream>
-# include <cstdlib>
-# include <cstring>
-# include <string>
-# include <unistd.h>
-# include <fcntl.h>
-# include <sys/types.h>
-# include <sys/socket.h>
-# include <netdb.h>
-# include <netinet/in.h>
-# include <stdexcept>
-# include <cerrno>
-# include <poll.h>
-# include <algorithm>
-# include <vector>
-# include <map>
-# include <sstream>
+#include "CommandFactory.hpp"
+#include "IrcClients.hpp"
+#include <iostream>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <cstdio>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <cstring>
+#include <cstdlib>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <poll.h>
+#include <vector>
+#include <utility>
+#include <map>
+#include <csignal>
+
+
+#define MAX_CON 100
+#define MAX_BUF 512
+#define HOSTNAME "127.0.0.1"
+#define IP_BIND "127.0.0.1"
+
+
+class CommandFactory;
+
+class Server
+{
+	public:
+		Server(int port, std::string password, IrcClients *ircClients, CommandFactory *commandFactory);
+		~Server();
+
+		void start(); // do all to start server
+		void createSocket();
+		void waitConnections();
+		void acceptConnection();
+		void readMessage(int indexFd);
+		void sendResponse(int clientSocket, std::string msg);
+		void cleanUp();
+
+	private:
+		int									portNb;
+		std::string							password;
+		std::string							ipAddress;
+		std::string							hostName;
+		int									socketNb;
+		std::vector<pollfd>					pollFdVector;
+		IrcClients 							*ircClients;
+		CommandFactory 						*commandFactory;
+
+
+		Server();
+		Server(Server const &);
+		Server &operator=(Server const &);
+};
+
 
 #endif
---
+++ src/Server.cpp
@@ -0,0 +1,247 @@
+
+#include "Server.hpp"
+#include "IrcClients.hpp"
+#include "CommandFactory.hpp"
+#include "IrcCommandParser.hpp"
+
+bool running = true;
+void signalHandler(int sigNum)
+{
+	(void)sigNum;
+	running = false;
+}
+
+Server::Server() {}
+
+Server::Server(Server const &) { }
+
+Server &Server::operator=(Server const &) {return *this;}
+
+Server::Server(int port, std::string password, IrcClients *ircClients, CommandFactory *commandFactory)
+	 : portNb(port),
+	  password(password),
+	  ipAddress(IP_BIND),
+	  hostName(HOSTNAME),
+	  socketNb(-1),
+	  pollFdVector(),
+	  ircClients(ircClients),
+	  commandFactory(commandFactory)
+{
+	std::cout << "Starting IRC Server ..." << std::endl;
+}
+
+Server::~Server() {
+	cleanUp();
+}
+
+void Server::start()
+{
+	createSocket();
+	waitConnections();
+}
+
+void Server::createSocket()
+{
+	int fd = socket(AF_INET, SOCK_STREAM, 0); // Creating the Socket: The socket is created using the socket() function with the parameters AF_INET (IPv4), SOCK_STREAM (TCP), and 0 (default protocol for TCP).
+    if (fd < 0)
+	{
+		std::cerr << "ERROR! Unable to create the socket!" << std::endl;
+		exit (1); // kill the program
+	}
+
+	/*
+	Setting SO_REUSEADDR: The socket option SO_REUSEADDR is set using the setsockopt() function. This allows the socket to reuse the local
+	address if it is already in use. The setsockopt() function is called with the parameters SOL_SOCKET (socket level), SO_REUSEADDR
+	(socket option), and a pointer to the value reuse (set to 1).
+	*/
+	int enable = 1;
+	if (setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, &enable, sizeof(enable)) < 0)
+	{
+		close (fd);
+		std::cerr << "ERROR! Socket was not cleared!" <<std::endl;
+		exit (1);
+	}
+
+	/*
+	O_NONBLOCK is a flag used to set a file descriptor, such as a socket, to non-blocking mode. When a file descriptor is in non-blocking mode,
+	operations on it, like reading or writing, will return immediately without waiting for the operation to complete. This is particularly useful
+	in network programming, where you don't want your application to be blocked waiting for I/O operations to complete.
+	*/
+	if (fcntl(fd, F_SETFL, O_NONBLOCK) < 0) {
+		close (fd);
+		std::cerr << "ERROR! Error setting socket to non-blocking mode!" << std::endl;
+		exit (1);
+	}
+
+	// Bind the Socket: The socket is bound to the configured address and port using the bind() function.
+	sockaddr_in serverAddr;
+    memset(&serverAddr, 0, sizeof(serverAddr));
+	serverAddr.sin_family = AF_INET;
+	serverAddr.sin_addr.s_addr = inet_addr(ipAddress.c_str());
+	serverAddr.sin_port = htons(portNb);
+	if (bind(fd, (sockaddr *)&serverAddr, sizeof(serverAddr)) < 0)
+	{
+		close(fd);
+		std::cerr << "Error binding socket" <<std::endl;
+		exit (1);
+	}
+
+ // Listen for Connections: The socket is set to listen for incoming connections using the listen() function, with a backlog of MAXCOUNT pending connections.
+	if (listen(fd, MAX_CON) < 0)
+	{
+		close(fd);
+		std::cerr << "Error listening on socket." <<std::endl;
+		exit (1);
+	}
+	socketNb = fd;
+}
+
+void Server::waitConnections()
+	// init pollfd struct and add server socket to the list
+{
+	pollfd serverPollfd;
+	std::string msg;
+    serverPollfd.fd = socketNb;
+    serverPollfd.events = POLLIN;
+    serverPollfd.revents = 0;
+    pollFdVector.push_back(serverPollfd);
+
+	std::cout << "Waiting for clients..." << std::endl;
+	signal(SIGINT, signalHandler);
+	//signal(SIGTSTP, signalHandler);
+
+	while (true)
+	{
+		if (running == false)
+			break ;
+		if (poll(pollFdVector.data(), pollFdVector.size(), 5) < 0)
+		{
+			cleanUp();
+			break ;
+		}
+		for (unsigned int i = 0; i < pollFdVector.size(); i++)
+		{
+			if (pollFdVector[i].revents & POLLIN) // check all sockets
+			{
+				if (pollFdVector[i].fd == socketNb) // check any info has been received from socket
+					acceptConnection();
+				else 							// the message is only read if the connection is acceptable
+					readMessage(i);
+			}
+			// verify if write/send a message is possiblr
+			if(pollFdVector[i].revents & POLLOUT)
+			{
+				Client *client = ircClients->getClient(pollFdVector[i].fd);
+				if (client != NULL && !client->pendingWrite.empty())
+				{
+					msg = client->pendingWrite.front();
+					send(pollFdVector[i].fd, msg.c_str(), msg.size(), 0);
+					client->pendingWrite.pop_front();
+				}
+			}
+        }
+	}
+}
+
+void Server::sendResponse(int clientSocket, std::string msg) {
+	Client *client = ircClients->getClient(clientSocket);
+	if (client != NULL)
+		client->pendingWrite.push_back(msg);
+}
+
+void Server::acceptConnection()
+{
+	// create info for new client and new socket
+	sockaddr_in clientAddr;
+	memset(&clientAddr, 0, sizeof(clientAddr));
+	socklen_t	clientSize = sizeof(clientAddr);
+
+	int clientSocket = accept(socketNb, (sockaddr *)&clientAddr, &clientSize);
+	if (clientSocket < 0) {
+		std::cerr << " Connection can not be established with new client" << std::endl;
+		return ;
+	}
+
+	// creation of the poll
+	pollfd clientPollfd;
+    clientPollfd.fd = clientSocket;
+    clientPollfd.events = POLLIN | POLLOUT;
+    clientPollfd.revents = 0;
+    pollFdVector.push_back(clientPollfd);
+
+    // add new client to ClientArray
+	std::string ipClient = inet_ntoa(clientAddr.sin_addr);
+	ircClients->createClient(clientSocket, ipClient);
+
+	std::cout <<"Client successfully connected on socket fd: " << clientSocket << std::endl;
+	sendResponse(clientSocket,  "Register the yourself is mandatory to use the server, by using: PASS, NICK and USER\r\n");
+}
+
+void Server::readMessage(int i)
+{
+	int clientFd = pollFdVector[i].fd;
+	char buffer[MAX_BUF];
+	memset(buffer, 0, sizeof(buffer));
+	int bytesRecv = recv(clientFd, buffer, sizeof(buffer), 0);
+	if (bytesRecv <= 0) {
+		if (bytesRecv == 0) // only when close terminal
+			std::cerr << "Client disconnected from socket fd: " << clientFd << std::endl;
+		else
+			std::cerr << "Connection Error!" << std::endl;
+
+		//  deleting client that disconected
+		if (!ircClients->removeClient(clientFd))
+		{
+			std::cerr << "Failed to remove client!" << std::endl;
+		}
+		std::vector<pollfd>::iterator it2;
+		for (it2 = pollFdVector.begin(); it2 != pollFdVector.end(); it2++)
+		{
+			if (it2->fd == clientFd)
+			{
+				pollFdVector.erase(it2);
+				break ;
+			}
+		}
+		return ;
+	}
+	std::cout << "Received: " << bytesRecv << " bytes Raw msg:" << std::string(buffer) << std::endl;
+	Client *currentClient = ircClients->getClient(clientFd);
+	if (currentClient != NULL)
+	{
+		currentClient->messageHandler(buffer);
+		if (currentClient->isMessageReady())
+		{
+			std::cout << "Message ready to be processed: " << currentClient->getEntireMessage() << std::endl;
+			IrcCommandParser parser = IrcCommandParser(std::string(currentClient->getEntireMessage()));
+			CommandType commandType = parser.getMessageType();
+			if (commandType != INVALID)
+			{
+				std::cout << "commandType " << commandType << std::endl;
+				ICommand *command = commandFactory->createCommand(commandType);
+				command->execute(clientFd, currentClient, this, parser.getPayload());
+				delete command;
+			}
+			else
+				std::cerr << "Invalid message received!" <<std::endl;
+		}
+	}
+	else
+		std::cerr << "Client not found!" <<std::endl;
+}
+
+void Server::cleanUp()
+{
+	// close the fds
+	if (!pollFdVector.empty())
+	{
+		for (unsigned int i = 0; i < pollFdVector.size(); i++)
+		{
+			if (pollFdVector[i].fd > 0) {
+				close(pollFdVector[i].fd);
+				pollFdVector.erase(pollFdVector.begin() + i);
+			}
+		}
+		pollFdVector.clear();
+	}
+}
