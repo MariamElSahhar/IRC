8dc7f4b7f8695009a97581db029e40eb9515c640
Paula Balbino <lola.paulinha@gmail.com>
2024-08-06 23:20:43 +0400

    fixed ping

--- src/Commands/CommandPing.cpp
+++ src/Commands/CommandPing.cpp
@@ -8,6 +8,7 @@ void CommandPing::execute(int &clientSocket,
                           Client *client,
                           Server *server,
                           std::vector<std::string> *param) {
+    (void) client;
 
 	std::string ping = "PONG " + param->at(0) + "\r\n";
 
