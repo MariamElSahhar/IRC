ec2597b8042d9eb418a3013b7a2a30d403d4332c
MIkamal88 <109902742+MIkamal88@users.noreply.github.com>
2024-08-19 12:58:04 +0400

    Removed comments

--- src/Commands/CommandCap.cpp
+++ src/Commands/CommandCap.cpp
@@ -13,10 +13,3 @@ void CommandCap::execute(int &clientSocket,
 
 	server->sendResponse(clientSocket, "CAP * LS :\r\n");
 }
-
-/*
-@MIkamal88 check this https://ircv3.net/specs/extensions/capability-negotiation.html
-I undestand that we need to display CAP * LS : instead of the message.
-
-means that we have no available capabilities
-*/
